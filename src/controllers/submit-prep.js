281f56869585fd9314dc5e08934028a8aa36d873
plasticneuron <plasticneuron@posteo.net>
2019-12-20 13:23:03 +0000

    make submit-prep work!

--- src/controllers/submit-prep.js
+++ src/controllers/submit-prep.js
@@ -1,11 +1,13 @@
 const { addPrep } = require("../controllers/queries/post-data");
 
 exports.post = (req, res) => {
+  console.log('submit-prep controller triggered')
   const newPrep = req.body;
   // const prepperId = req.session.id
   const prepperId = 1; // hard code for now (no session implemented yet)
   addPrep(prepperId, newPrep)
     .then(() => {
+      console.log('addPrep in submit-prep fulfills')
       res.redirect("/profile");
     })
     .catch(err => {
